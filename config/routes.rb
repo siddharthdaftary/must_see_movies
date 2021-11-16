79c0afab503324d75dbc56bb74e1fc9b362508f1
anonymous anonymous <siddharth.daftary@kellogg.northwestern.edu>
2021-11-16 04:58:45 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "actors#index"
   resources :reviews
   resources :bookmarks
