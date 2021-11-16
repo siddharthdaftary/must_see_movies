df943849ec8a0e2c8c46cca420af5e348509bd6d
anonymous anonymous <siddharth.daftary@kellogg.northwestern.edu>
2021-11-16 04:59:44 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,15 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :description, :string
+  attribute :year, :date
+  attribute :duration, :string
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
